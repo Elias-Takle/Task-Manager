29fe05473e2881913093852f4a79f178bc032d84
Elias-Takle <eliastakie424@gmail.com>
2025-05-12 14:18:14 +0300

    modified input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import ManagerDashbord from "./pages/ManagerDashbord";
-import EmployeesDashbord from "./pages/EmployeesDashbord";
+// import ManagerDashbord from "./pages/ManagerDashbord";
+// import EmployeesDashbord from "./pages/EmployeesDashbord";
 import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Login />
       <Routes>
-        <Route path="/managerdashbord" element={<ManagerDashbord />} />
-        <Route path="/employeedashbord" element={<EmployeesDashbord />} />
+        <Route path="/" element={<Login />} />
+        {/* <Route path="/managerdashbord" element={<ManagerDashbord />} />
+        <Route path="/employeedashbord" element={<EmployeesDashbord />} /> */}
       </Routes>
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,95 +1,88 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const handleLogin = async (e) => {
+  const [form, setForm] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5173/api/login", {
-        email,
-        password,
-      });
-
-      const { token, user } = res.data;
-      localStorage.setItem("token", token);
-      localStorage.setItem("user", user.role);
-      // redirect to the dashboard based on the user role
-      if ((user, role === "manager")) {
-        navigate("/manager/dashboard");
-      } else {
-        navigate("/employee/dashboard");
-      }
+      const response = await axios.post(
+        "http://localhost:3000/api/register",
+        form
+      );
+      alert(response.data.message);
     } catch (error) {
-      alert("Login failed: " + err.response?.data?.message || err.message);
+      console.error("Error:", error);
+      alert("Submission failed");
     }
   };
+
   return (
-    <div className=" w-[500px] h-100vh mx-auto  my-50 ">
-      <form onSubmit={handleLogin} className="w-[500px] ">
+    <div className="w-[500px] mx-auto my-30">
+      <form className="w-[500px]" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="fname"
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+          placeholder="Enter Your First Name"
+          className="w-[500px] h-15 bg-gray-500 rounded-lg px-2 my-5"
+        />
+        <br />
+        <input
+          type="text"
+          name="lname"
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+          placeholder="Enter Your Last Name"
+          className="w-[500px] h-15 bg-gray-500 rounded-lg px-2 my-5"
+        />
+        <br />
         <input
           type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter Your email"
-          autofill="off"
-          className="w-[500px] h-15 bg-gray-500 rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent px-2 my-5"
+          name="email"
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+          placeholder="Enter Your Email"
+          className="w-[500px] h-15 bg-gray-500 rounded-lg px-2 my-5"
         />
         <br />
         <input
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter Your password"
-          className="w-[500px] h-15 bg-gray-500 rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent px-2 my-5"
+          name="password"
+          onChange={(e) =>
+            setForm({ ...form, [e.target.name]: e.target.value })
+          }
+          placeholder="Enter Your Password"
+          className="w-[500px] h-15 bg-gray-500 rounded-lg px-2 my-5"
         />
         <br />
-        <button
-          type="submit"
-          className="flex w-20 h-9 bg-rose-500  rounded-lg hover:bg-rose-600 text-white font-bold mx-auto items-center justify-center"
-        >
-          Sign In
-        </button>
-        <br />
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="w-20 h-9 bg-rose-500 rounded-lg text-white font-bold"
+          >
+            Manager
+          </button>
+          <button
+            type="submit"
+            className="w-20 h-9 bg-rose-500 rounded-lg text-white font-bold"
+          >
+            Employee
+          </button>
+        </div>
       </form>
     </div>
   );
 }
 
 export default Login;
-
-{
-  /* <input
-          type="text"
-          name="fname"
-          placeholder="Enter Your first Name"
-          className="w-[500px] h-10 bg-gray-500 rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent px-2 my-5"
-        />
-        <br />
-        <input
-          type="text"
-          name="lname"
-          placeholder="Enter Your last Name"
-          className="w-[500px] h-10 bg-gray-500 rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent px-2 my-5"
-        />
-        <br /> */
-}
-
-//   <div className="flex justify-between">
-//   <button
-//     onClick={handleClick}
-//     className="w-20 h-9 bg-rose-500  rounded-lg hover:bg-rose-600 text-white font-bold"
-//   >
-//     Manager
-//   </button>
-//   <button
-//     onClick={handleClick}
-//     className="w-20 h-9 bg-rose-500  rounded-lg hover:bg-rose-600 text-white font-bold"
-//   >
-//     Employee
-//   </button>
-// </div>
